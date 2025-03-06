c5f7fb29140ca9e63ba3dc09162be6cd5b5c4aaa
Anoj <anoj@newagesysit.com>
2025-03-06 11:52:42 +0530

    bug fix

--- src/script.js
+++ src/script.js
@@ -459,7 +459,7 @@ $(document).ready(function () {
   var slider_4 = $(".dev-services-slider_4");
   slider_4.slick({
     infinite: true,
-    slidesToShow: 4,
+    slidesToShow: 5,
     slidesToScroll: 1,
     prevArrow: $(".ds_prev_new"),
     nextArrow: $(".ds_next_new"),
